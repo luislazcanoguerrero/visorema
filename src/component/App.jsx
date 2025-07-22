53a98876e9dc10619a531690d264e91d0adbf93a
Luis Lazcano Guerrero    <llazcano@dgac.gob.cl>
2025-07-22 01:43:22 -0400

    visor ema

---
+++ src/component/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    const momentoActual = '01-01-2024'
+    return (
+        <>
+            <h1>Visor Componente</h1>
+            <h2>{momentoActual}</h2>
+        </>
+    )
+}
